91af74981a33a166681b8100ed9a4bcf0c237979
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-28 19:01:54 +0200

    puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# make changes to config file using puppet
+
+$content = 'Host 349756-web-01
+	HostName 35.175.104.84
+	User ubuntu
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no'
+
+file { '~/.ssh/config':
+  ensure  => file,
+  content => $content,
+}
