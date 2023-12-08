eaf01daa72438c05b0ccaf299ca4b598dd7454e8
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-12-08 02:19:09 +0200

    pup fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,24 @@
-# Creates a custom HTTP header response with Puppet.
-exec { 'command':
-  command  => 'sudo apt-get update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/server_name _;/a \\n\tadd_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
-  sudo service nginx restart',
+# Installs a Nginx server with custom HTTP header
+
+exec {'update':
   provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider => shell,
+  command  => 'sudo sed -i "/server_name _;/a \\n\tadd_header X-Served-By $hostname;" /etc/nginx/sites-available/default',
+  before   => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
