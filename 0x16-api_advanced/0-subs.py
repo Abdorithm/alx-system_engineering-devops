ae1cab9dcd6419716907a9b7b417338440e70ef5
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-02-15 15:29:40 +0200

    hot fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return number of subs of a subreddit"""
-    session = requests.Session()
-    session.max_redirects = 0
     try:
         response = requests.get('https://www.reddit.com/r/{}/about.json'
                                 .format(subreddit),
-                                headers={'User-agent': 'Mozilla/5.0'})
-    except requests.exceptions.RequestException as e:
+                                headers={'User-agent': 'Mozilla/5.0'},
+                                allow_redirects=False)
+    except requests.exceptions.RequestException:
         return 0
-    data = response.json().get('data')
-    return data.get('subscribers')
+    try:
+        data = response.json().get('data')
+        n = data.get('subscribers')
+    except (json.decoder.JSONDecodeError, AttributeError):
+        return 0
+    return n
